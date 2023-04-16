771dfedff723b6202a29fd232f7279bde7ef1827
nitayGitHub <nitaylevi1998@gmail.com>
2023-04-16 10:47:44 +0300

    firstUpdate

---
+++ sources/Fraction.hpp
@@ -0,0 +1,24 @@
+#ifndef FRAC_H
+#define FRAC_H
+#include <iostream>
+using namespace std;
+namespace ariel
+{
+class Fraction
+{
+private:
+    /* data */
+public:
+    Fraction(/* args */);
+};
+
+Fraction::Fraction(/* args */)
+{
+}
+
+
+}
+
+
+
+#endif
