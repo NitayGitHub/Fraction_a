24ff56e8d7e82101515165439f31f00473285f8d
nitayGitHub <nitaylevi1998@gmail.com>
2023-04-16 17:27:51 +0300

    comit2

--- sources/Fraction.hpp
+++ sources/Fraction.hpp
@@ -7,14 +7,45 @@ namespace ariel
 class Fraction
 {
 private:
-    /* data */
+    int deno; // denominator
+    int nume; // numerator
+    void gcd(int a, int b);
 public:
-    Fraction(/* args */);
-};
+    // constructors
+    Fraction(int numerator, int denominator);
+    Fraction(int numerator);
+    
+    // functions
+    void SimplfyFraction();
 
-Fraction::Fraction(/* args */)
-{
-}
+    // operators
+    Fraction operator+(const Fraction &other) const;
+    friend Fraction operator+(const float &f, const Fraction &frac);
+    Fraction operator-(const Fraction &other) const;
+    friend Fraction operator-(const float &f, const Fraction &frac);
+    Fraction operator*(const Fraction &other) const;
+    friend Fraction operator*(const float &f, const Fraction &frac);
+    Fraction operator/(const Fraction &other) const;
+    friend Fraction operator/(const float &f, const Fraction &frac);
+    bool operator==(const Fraction &other) const;
+    friend bool operator==(const float &f, const Fraction &frac);
+    bool operator>(const Fraction &other) const;
+    friend bool operator>(const float &f, const Fraction &frac);
+    bool operator<(const Fraction &other) const;
+    friend bool operator<(const float &f, const Fraction &frac);
+    bool operator>=(const Fraction &other) const;
+    friend bool operator>=(const float &f, const Fraction &frac);
+    bool operator<=(const Fraction &other) const;
+    friend bool operator<=(const float &f, const Fraction &frac);
+    Fraction& operator++(int increment_flag);
+    Fraction& operator++();
+    Fraction& operator--(int increment_flag);
+    Fraction& operator--();
+    friend ostream& operator<< (ostream& os, const Fraction& frac);
+    friend istream& operator>> (istream& is, Fraction& frac);
+
+    
+};
 
 
 }
